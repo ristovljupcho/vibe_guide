efdd47bf6c79a2e7d4e61b94023265a216e2906f
ristovljupcho <ristov.ljupcho@gmail.com>
2024-12-11 00:08:02 +0100

    Commit of Spring Boot Application with CI.

---
+++ src/test/java/com/vibe_guide/VibeGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibe_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
