defe50603c41380c89142be6f42e5d835c5ae352
ristovljupcho <ristov.ljupcho@gmail.com>
2024-12-11 00:08:47 +0100

    Commit of Spring Boot Application with CI implementation.

--- src/main/java/com/vibe_guide/VibeGuideApplication.java
+++ src/main/java/com/vibe_guide/VibeGuideApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VibeGuideApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VibeGuideApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VibeGuideApplication.class, args);
+    }
 
 }
