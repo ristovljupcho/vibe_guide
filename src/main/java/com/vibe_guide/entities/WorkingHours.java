3ab4255973a308053fcd98d55b7a8eea5adeaa37
ristovljupcho <ristov.ljupcho@gmail.com>
2024-12-23 23:55:12 +0100

    Adding new entity WorkingHours and WorkingHoursId class representing id.

---
+++ src/main/java/com/vibe_guide/entities/WorkingHours.java
@@ -0,0 +1,52 @@
+package com.vibe_guide.entities;
+
+import com.vibe_guide.entities.composite_keys.WorkingHoursId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalTime;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@IdClass(WorkingHoursId.class)
+public class WorkingHours {
+    @Id
+    private String dayOfWeek;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "local_profile_id")
+    @ToString.Exclude
+    private LocalProfile localProfile;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        WorkingHours that = (WorkingHours) o;
+        return Objects.equals(dayOfWeek, that.dayOfWeek) &&
+                Objects.equals(localProfile, that.localProfile);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dayOfWeek, localProfile);
+    }
+}
