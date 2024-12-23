5e782823757375413e534f9d72c2c9634792a0f5
ristovljupcho <ristov.ljupcho@gmail.com>
2024-12-23 19:57:06 +0100

    Adding new LocalProfile entity.

---
+++ src/main/java/com/vibe_guide/entities/LocalProfile.java
@@ -0,0 +1,61 @@
+package com.vibe_guide.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+public class LocalProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private String mapsUri;
+
+    private String phoneNumber;
+
+    private String address;
+
+    private double rating = 0.0;
+
+    private String manuLink;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trait_id")
+    @ToString.Exclude
+    private Trait trait;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LocalProfile that = (LocalProfile) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
