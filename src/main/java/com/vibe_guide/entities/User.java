fd5371feea00f45d97a184e183adb4363708768f
ristovljupcho <ristov.ljupcho@gmail.com>
2024-12-26 13:55:42 +0100

    Adding @Table to User class and code refactoring.

--- src/main/java/com/vibe_guide/entities/User.java
+++ src/main/java/com/vibe_guide/entities/User.java
@@ -1,8 +1,18 @@
 package com.vibe_guide.entities;
 
 import com.vibe_guide.enums.Role;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -13,8 +23,8 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
+@Table(name = "user_table")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -27,7 +37,6 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
-
 
     @Override
     public boolean equals(Object o) {
