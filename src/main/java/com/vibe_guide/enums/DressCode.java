0e01e5b55b98c6e25b4ce2854a48ae27c6c7b5a4
ristovljupcho <ristov.ljupcho@gmail.com>
2025-01-02 15:37:37 +0100

    Code reformatting.

--- src/main/java/com/vibe_guide/enums/DressCode.java
+++ src/main/java/com/vibe_guide/enums/DressCode.java
@@ -1,7 +1,6 @@
 package com.vibe_guide.enums;
 
 public enum DressCode {
-    NOT_DEFINED,
     CLASSY,
     CASUAL
 }
--- src/main/java/com/vibe_guide/enums/MusicType.java
+++ src/main/java/com/vibe_guide/enums/MusicType.java
@@ -1,10 +1,7 @@
 package com.vibe_guide.enums;
 
 public enum MusicType {
-    NOT_DEFINED,
-    HOUSE_AND_TEHNO,
     ROCK,
     POP,
     RAP,
-    COZY_LOFI
 }
