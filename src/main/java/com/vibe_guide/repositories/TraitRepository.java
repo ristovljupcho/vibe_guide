d67e9eb36baf7197691cf54445695e933e40d74e
ristovljupcho <ristov.ljupcho@gmail.com>
2025-02-23 09:49:35 +0100

    Implementing methods in TraitQueryService.

--- src/main/java/com/vibe_guide/repositories/TraitRepository.java
+++ src/main/java/com/vibe_guide/repositories/TraitRepository.java
@@ -1,9 +1,23 @@
 package com.vibe_guide.repositories;
 
 import com.vibe_guide.entities.Trait;
+import com.vibe_guide.enums.TraitType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface TraitRepository extends JpaRepository<Trait, UUID> {
+
+    @Query("select trait " +
+            "from Trait trait ")
+    Page<Trait> getPaginatedTraits(Pageable pageable);
+
+    @Query("select trait " +
+            "from Trait trait " +
+            "where trait.traitType = :traitType")
+    Page<Trait> getPaginatedTraitsByTraitType(@Param("traitType") TraitType traitType, Pageable pageable);
 }
---
+++ src/main/java/com/vibe_guide/services/impl/TraitQueryServiceImpl.java
@@ -0,0 +1,54 @@
+package com.vibe_guide.services.impl;
+
+import com.vibe_guide.converters.TraitConverter;
+import com.vibe_guide.dtos.TraitPreviewResponseDTO;
+import com.vibe_guide.entities.Trait;
+import com.vibe_guide.enums.SortDirection;
+import com.vibe_guide.enums.TraitSortBy;
+import com.vibe_guide.enums.TraitType;
+import com.vibe_guide.repositories.TraitRepository;
+import com.vibe_guide.services.TraitQueryService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TraitQueryServiceImpl implements TraitQueryService {
+
+    private final TraitRepository traitRepository;
+    private final TraitConverter traitConverter;
+
+    /**
+     * Retrieves {@link Trait} objects using pagination. Filtering is enabled using {@link TraitType} which will
+     * display {@link Trait} objects with a certain type. Sorting by name or default (UUID), in ASC or DESC order.
+     *
+     * @param traitType     can be <u><i>null</i></u>, used for filtering {@link Trait} objects.
+     * @param sortBy        used for sorting, default trait sort criteria is <b><i>TYPE</i></b> from enum
+     *                      {@link TraitSortBy}.
+     * @param sortDirection used for sorting direction, default sort direction is <b><i>ASC</i></b> from enum
+     *                      {@link SortDirection}.
+     * @param page          page number.
+     * @param size          size of the page to be returned.
+     * @return A {@link Page} containing {@link TraitPreviewResponseDTO} objects.
+     */
+    @Override
+    public Page<TraitPreviewResponseDTO> getPaginatedTraits(TraitType traitType, TraitSortBy sortBy,
+                                                            SortDirection sortDirection, int page, int size) {
+        String sortField = switch (sortBy) {
+            case DEFAULT -> "id";
+            case NAME -> "name";
+        };
+        Sort sort =
+                Sort.by(sortDirection == SortDirection.DESC ? Sort.Order.desc(sortField) : Sort.Order.asc(sortField));
+        PageRequest pageRequest = PageRequest.of(page, size, sort);
+        Page<Trait> traitPage;
+        if (traitType == null)
+            traitPage = traitRepository.getPaginatedTraits(pageRequest);
+        else traitPage = traitRepository.getPaginatedTraitsByTraitType(traitType, pageRequest);
+
+        return traitPage.map(traitConverter::toTraitPreviewResponseDTO);
+    }
+}
