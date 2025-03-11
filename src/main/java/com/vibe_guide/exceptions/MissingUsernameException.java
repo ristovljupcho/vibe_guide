c2a203ad70f83278a2f355c8793657e9e95d764d
Dzoki12 <petrovski.gjorgji21@gmail.com>
2025-03-11 23:43:34 +0100

    add javadoc to userConverter method and class and removing unnecessary exception

--- src/main/java/com/vibe_guide/exceptions/MissingUsernameException.java
+++
@@ -1,7 +0,0 @@
-package com.vibe_guide.exceptions;
-
-public class MissingUsernameException extends CustomNotFoundException {
-    public MissingUsernameException() {
-        super("Username is required.");
-    }
-}
