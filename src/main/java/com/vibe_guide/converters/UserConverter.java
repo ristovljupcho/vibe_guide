75840c96f8f4afab2f47265a4a11d89c5c69648b
Dzoki12 <petrovski.gjorgji21@gmail.com>
2025-03-11 15:11:09 +0100

    Fixed UserConverter.java and UserPreviewResponseDTO.java

--- src/main/java/com/vibe_guide/converters/UserConverter.java
+++ src/main/java/com/vibe_guide/converters/UserConverter.java
@@ -5,20 +5,17 @@ import com.vibe_guide.entities.User;
 import com.vibe_guide.enums.Role;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Component
 public class UserConverter {
     public UserPreviewResponseDTO toUserPreviewResponseDTO(User user) {
-        UUID userId = UUID.randomUUID();//shouldn't we use the actual id? .getID()?
+        UUID userId = user.getId();
         String username = user.getUsername();
         String name = user.getName();
         String email = user.getEmail();
-        LocalDateTime createdAt = user.getCreatedAt();
-        LocalDateTime updatedAt = user.getUpdatedAt();
         Role role = user.getRole();
 
-        return new UserPreviewResponseDTO(userId, username, name, email, createdAt, updatedAt, role);
+        return new UserPreviewResponseDTO(userId, username, name, email, role);
     }
 }
--- src/main/java/com/vibe_guide/dtos/UserPreviewResponseDTO.java
+++ src/main/java/com/vibe_guide/dtos/UserPreviewResponseDTO.java
@@ -2,17 +2,13 @@ package com.vibe_guide.dtos;
 
 import com.vibe_guide.enums.Role;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record UserPreviewResponseDTO(
-        UUID id,
+        UUID userId,
         String username,
         String name,
         String email,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt,
         Role role
 ) {
-
 }
