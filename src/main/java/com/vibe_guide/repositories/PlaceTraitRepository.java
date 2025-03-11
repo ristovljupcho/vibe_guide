bf02fe50eb799da185748e338c6d239fcd105d63
ristovljupcho <ristov.ljupcho@gmail.com>
2025-03-11 14:21:45 +0100

    Implementing new method in PlaceTraitRepository.

--- src/main/java/com/vibe_guide/repositories/PlaceTraitRepository.java
+++ src/main/java/com/vibe_guide/repositories/PlaceTraitRepository.java
@@ -1,8 +1,19 @@
 package com.vibe_guide.repositories;
 
 import com.vibe_guide.entities.PlaceTrait;
+import com.vibe_guide.entities.Trait;
 import com.vibe_guide.entities.composite_keys.PlaceTraitId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface PlaceTraitRepository extends JpaRepository<PlaceTrait, PlaceTraitId> {
+
+    @Query("SELECT pt.trait " +
+            "FROM PlaceTrait AS pt " +
+            "WHERE pt.place.id = :placeId " +
+            "ORDER BY pt.priority DESC, pt.likeCounter DESC")
+    List<Trait> getTraitsForPlaceCarousel(UUID placeId);
 }
