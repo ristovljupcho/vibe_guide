3534252579fc2264795fc79604686967e5c1c7b3
Negrioska <leona.negrioska@gmail.com>
2025-03-12 19:37:58 +0100

    Created new method findPastEvents in EventRepository.

--- src/main/java/com/vibe_guide/repositories/EventRepository.java
+++ src/main/java/com/vibe_guide/repositories/EventRepository.java
@@ -3,8 +3,17 @@ package com.vibe_guide.repositories;
 import com.vibe_guide.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+import org.springframework.data.jpa.repository.Query;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID>, JpaSpecificationExecutor<Event> {
+    @Query ("SELECT e " +
+            "FROM Event AS e " +
+            "WHERE e.place.id = :placeId " +
+            "AND e.endDate <= :now " +
+            "AND e.startDate >= :oneMonthAgo " +
+            "ORDER BY e.startDate DESC")
+    List<Event> findPastEvents (UUID placeId, LocalDateTime now, LocalDateTime oneMonthAgo);
 }
