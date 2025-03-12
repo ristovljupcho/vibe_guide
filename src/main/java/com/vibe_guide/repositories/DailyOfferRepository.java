949177d641ead875aab372ba37d86fb92352b6f0
Negrioska <leona.negrioska@gmail.com>
2025-03-12 19:26:52 +0100

    Implemented methods in DailyOfferRepository for filtering by placeId and by today's date.

--- src/main/java/com/vibe_guide/repositories/DailyOfferRepository.java
+++ src/main/java/com/vibe_guide/repositories/DailyOfferRepository.java
@@ -2,9 +2,21 @@ package com.vibe_guide.repositories;
 
 import com.vibe_guide.entities.DailyOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface DailyOfferRepository extends JpaRepository<DailyOffer, UUID> {
-    List<DailyOffer> findByPlaceId(UUID placeId);
+    @Query("SELECT do " +
+            "FROM DailyOffer AS do " +
+            "WHERE do.startDate = :today " +
+            "AND do.place.id = :placeId")
+    List<DailyOffer> findTodayDailyOffersByPlaceId(LocalDateTime today, UUID placeId);
+
+    @Query("SELECT do " +
+            "FROM DailyOffer AS do " +
+            "WHERE do.startDate = :today ")
+    List<DailyOffer> findTodayDailyOffers(LocalDateTime today);
 }
