26a1bd524b953560f98d6ef80fa688dba0b2af05
Negrioska <leona.negrioska@gmail.com>
2025-03-13 01:47:14 +0100

    Created repository EventGalleryRepository for EventGallery entity.

---
+++ src/main/java/com/vibe_guide/repositories/EventGalleryRepository.java
@@ -0,0 +1,11 @@
+package com.vibe_guide.repositories;
+
+import com.vibe_guide.entities.EventGallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EventGalleryRepository extends JpaRepository<EventGallery, UUID> {
+    List<EventGallery> findByEventId(UUID eventId);
+}
