725b7cad1ac44091a1715e8de09a5943c6425bc6
Negrioska <leona.negrioska@gmail.com>
2025-03-20 19:03:38 +0100

    In DailyOfferRepository changed the queries to get the offers: startDay >= :today

--- src/main/java/com/vibe_guide/repositories/DailyOfferRepository.java
+++ src/main/java/com/vibe_guide/repositories/DailyOfferRepository.java
@@ -13,12 +13,12 @@ public interface DailyOfferRepository extends JpaRepository<DailyOffer, UUID> {
             "FROM DailyOffer AS do " +
             "WHERE do.startDate = :today " +
             "AND do.endDate >= :today " +
-            "AND do.place.id = :placeId")
+            "AND do.place.id <= :placeId")
     List<DailyOffer> findTodayDailyOffersByPlaceId(LocalDateTime today, UUID placeId);
 
     @Query("SELECT do " +
             "FROM DailyOffer AS do " +
-            "WHERE do.startDate = :today " +
+            "WHERE do.startDate >= :today " +
             "AND do.endDate >= :today")
     List<DailyOffer> findTodayDailyOffers(LocalDateTime today);
 }
