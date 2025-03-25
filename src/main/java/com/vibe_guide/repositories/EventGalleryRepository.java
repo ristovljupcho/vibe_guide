c7b9a367265f09b045efaa599f2997ceb47e2178
Negrioska <leona.negrioska@gmail.com>
2025-03-25 14:46:33 +0100

    Renamed method in EventGalleryRepository

--- src/main/java/com/vibe_guide/repositories/EventGalleryRepository.java
+++ src/main/java/com/vibe_guide/repositories/EventGalleryRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface EventGalleryRepository extends JpaRepository<EventGallery, UUID> {
-    List<EventGallery> findByEventId(UUID eventId);
+    List<EventGallery> findAllByEventId(UUID eventId);
 }
