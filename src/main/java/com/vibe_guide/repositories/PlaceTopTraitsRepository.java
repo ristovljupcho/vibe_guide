f8f047232e5e46e6c6517b791cf910e4d5f8e318
ristovljupcho <ristov.ljupcho@gmail.com>
2025-03-26 16:33:07 +0100

    Added repository for PlaceTopTraits materialized view.

---
+++ src/main/java/com/vibe_guide/repositories/PlaceTopTraitsRepository.java
@@ -0,0 +1,25 @@
+package com.vibe_guide.repositories;
+
+import com.vibe_guide.entities.views.PlaceTopTraits;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PlaceTopTraitsRepository extends JpaRepository<PlaceTopTraits, UUID> {
+
+    @Query("SELECT p FROM PlaceTopTraits p")
+    Page<PlaceTopTraits> findAllPaginated(Pageable pageable);
+
+    @Query("SELECT p FROM PlaceTopTraits p " +
+            "WHERE (SELECT COUNT(DISTINCT t.id) FROM PlaceTrait pt JOIN pt.trait t " +
+            "       WHERE pt.place.id = p.placeId AND t.name IN :traits) = :traitsSize")
+    Page<PlaceTopTraits> findAllByTraitsPaginated(
+            @Param("traits") List<String> traits,
+            @Param("traitsSize") int traitsSize,
+            Pageable pageable);
+}
