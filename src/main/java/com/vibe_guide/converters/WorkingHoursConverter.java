73330bf2dc347b03b240f32a5c2a2cc77f75071c
ristovljupcho <ristov.ljupcho@gmail.com>
2025-03-30 21:09:24 +0200

    Adding converter to convert from WorkingHours to WorkingHoursResponseDTO.

---
+++ src/main/java/com/vibe_guide/converters/WorkingHoursConverter.java
@@ -0,0 +1,32 @@
+package com.vibe_guide.converters;
+
+import com.vibe_guide.dtos.DailyOfferResponseDTO;
+import com.vibe_guide.dtos.WorkingHoursResponseDTO;
+import com.vibe_guide.entities.WorkingHours;
+import com.vibe_guide.enums.DayOfWeek;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+/**
+ * <ul>
+ *     <li>This class is responsible for providing conversion methods from {@link WorkingHours} entity to Data Transfer
+ *     Objects and vice versa.</li>
+ * </ul>
+ */
+@Component
+public class WorkingHoursConverter {
+    /**
+     * Converts a {@link WorkingHours} entity to a {@link WorkingHoursResponseDTO} entity.
+     *
+     * @param workingHours {@link WorkingHours} entity to convert.
+     * @return {@link DailyOfferResponseDTO} converted entity.
+     */
+    public WorkingHoursResponseDTO toWorkingHoursResponseDTO(WorkingHours workingHours) {
+        DayOfWeek dayOfWeek = workingHours.getDayOfWeek();
+        LocalTime startTime = workingHours.getStartTime();
+        LocalTime endTime = workingHours.getEndTime();
+
+        return new WorkingHoursResponseDTO(dayOfWeek, startTime, endTime);
+    }
+}
