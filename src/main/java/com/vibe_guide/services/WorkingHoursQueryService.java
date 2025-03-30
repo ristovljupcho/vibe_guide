b70d4f03b76b37b733bca3f7b0db28344f3a852b
ristovljupcho <ristov.ljupcho@gmail.com>
2025-03-30 23:59:06 +0200

    Added new getMissingWorkingHoursDaysForPlace method and helper method.

--- src/main/java/com/vibe_guide/services/WorkingHoursQueryService.java
+++ src/main/java/com/vibe_guide/services/WorkingHoursQueryService.java
@@ -1,5 +1,6 @@
 package com.vibe_guide.services;
 
+import com.vibe_guide.dtos.WorkingHoursMissingDaysResponseDTO;
 import com.vibe_guide.dtos.WorkingHoursResponseDTO;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.UUID;
 
 public interface WorkingHoursQueryService {
     List<WorkingHoursResponseDTO> getWeeklyWorkingHoursForPlace(UUID placeId);
+
+    List<WorkingHoursMissingDaysResponseDTO> getMissingWorkingHoursDaysForPlace(UUID placeId);
 }
