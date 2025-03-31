deaae084837a5ed0c4d9eb3e012054e1643f6c67
ristovljupcho <ristov.ljupcho@gmail.com>
2025-03-31 17:45:52 +0200

    Modified PlaceAdmin to have single composite key. Added new class PlaceAdminId implementing the new composite key. Added new attribute createdOn.

--- src/main/java/com/vibe_guide/repositories/PlaceAdminRepository.java
+++ src/main/java/com/vibe_guide/repositories/PlaceAdminRepository.java
@@ -1,9 +1,8 @@
 package com.vibe_guide.repositories;
 
 import com.vibe_guide.entities.PlaceAdmin;
+import com.vibe_guide.entities.composite_keys.PlaceAdminId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface PlaceAdminRepository extends JpaRepository<PlaceAdmin, UUID> {
+public interface PlaceAdminRepository extends JpaRepository<PlaceAdmin, PlaceAdminId> {
 }
