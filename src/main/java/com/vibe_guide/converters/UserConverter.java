1e4f78a3abfc600bd88ad94e6e35fae800816d53
ristovljupcho <ristov.ljupcho@gmail.com>
2025-04-02 12:46:19 +0200

    Added new DTO named PlaceAdminResponseDTO and converter in the UserConverter class.

--- src/main/java/com/vibe_guide/converters/UserConverter.java
+++ src/main/java/com/vibe_guide/converters/UserConverter.java
@@ -1,16 +1,18 @@
 package com.vibe_guide.converters;
 
+import com.vibe_guide.dtos.PlaceAdminResponseDTO;
 import com.vibe_guide.dtos.UserPreviewResponseDTO;
 import com.vibe_guide.entities.User;
 import com.vibe_guide.enums.Role;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+
 /**
  * <ul>
  * <li>
- *This class is responsible for providing conversion methods from {@link User} entity
- *to Data Transfer Objects and vice versa.
+ * This class is responsible for providing conversion methods from {@link User} entity
+ * to Data Transfer Objects and vice versa.
  * </li>
  * </ul>
  */
@@ -30,5 +32,18 @@ public class UserConverter {
         Role role = user.getRole();
 
         return new UserPreviewResponseDTO(userId, username, name, email, role);
+    }
+
+    /**
+     * * Converts a {@link User} entity to a {@link PlaceAdminResponseDTO} entity.
+     *
+     * @param user {@link User} entity to convert.
+     * @return The converted {@link PlaceAdminResponseDTO} entity.
+     */
+    public PlaceAdminResponseDTO toPlaceAdminResponseDTO(User user) {
+        UUID userId = user.getId();
+        String email = user.getEmail();
+
+        return new PlaceAdminResponseDTO(userId, email);
     }
 }
