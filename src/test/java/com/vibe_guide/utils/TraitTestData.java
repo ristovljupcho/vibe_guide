207dce1f4da3a2aff8469f83229a9ad390ace8f1
ristovljupcho <ristov.ljupcho@gmail.com>
2025-04-05 18:14:25 +0200

    Added new data used for TraitManagementServiceImplTest.

--- src/test/java/com/vibe_guide/utils/TraitTestData.java
+++ src/test/java/com/vibe_guide/utils/TraitTestData.java
@@ -1,6 +1,8 @@
 package com.vibe_guide.utils;
 
+import com.vibe_guide.dtos.TraitInsertRequestDTO;
 import com.vibe_guide.dtos.TraitResponseDTO;
+import com.vibe_guide.dtos.TraitUpdateRequestDTO;
 import com.vibe_guide.entities.Trait;
 import com.vibe_guide.enums.TraitType;
 import com.vibe_guide.enums.sorting.TraitSortBy;
@@ -16,14 +18,18 @@ import java.util.UUID;
 
 @UtilityClass
 public class TraitTestData {
-    private static final UUID TRAIT_ID = UUID.fromString("6a3e9932-4802-4815-8de8-2f1e99bdf841");
-    private static final String TRAIT_NAME = "Trait 1";
     private static final Sort TRAIT_SORT = Sort.by(Sort.Direction.ASC, "name");
 
+    public static final UUID TRAIT_ID = UUID.fromString("6a3e9932-4802-4815-8de8-2f1e99bdf841");
     public static final PageRequest TRAIT_PAGE_REQUEST =
             PageRequest.of(SharedTestData.PAGE, SharedTestData.SIZE, TRAIT_SORT);
+    public static final String TRAIT_NAME = "Trait 1";
     public static final TraitType TRAIT_TYPE = TraitType.MUSIC_TYPE;
     public static final TraitSortBy TRAIT_SORT_BY = TraitSortBy.NAME;
+    public static final String TRAIT_INSERT_MESSAGE =
+            "Trait of type " + TRAIT_TYPE + " and with name " + TRAIT_NAME + " successfully inserted.";
+    public static final String TRAIT_UPDATE_MESSAGE = "Trait with id " + TRAIT_ID + " successfully updated.";
+    public static final String TRAIT_DELETE_MESSAGE = "Trait with id " + TRAIT_ID + " successfully deleted.";
 
     public static Page<Trait> getPageTraits() {
         Trait trait1 = new Trait(
@@ -35,19 +41,23 @@ public class TraitTestData {
         Trait trait2 = new Trait(
                 UUID.fromString("6a3e9932-4802-4815-8de8-2f1e99bdf842"),
                 TRAIT_TYPE,
-                TRAIT_NAME,
+                "Trait 2",
                 new HashSet<>()
         );
         Trait trait3 = new Trait(
                 UUID.fromString("6a3e9932-4802-4815-8de8-2f1e99bdf843"),
                 TRAIT_TYPE,
-                TRAIT_NAME,
+                "Trait 3",
                 new HashSet<>()
         );
 
         List<Trait> traits = List.of(trait1, trait2, trait3);
 
         return new PageImpl<>(traits);
+    }
+
+    public static Trait getTrait() {
+        return getPageTraits().getContent().getFirst();
     }
 
     public static Page<TraitResponseDTO> getPageTraitResponseDTOs() {
@@ -59,16 +69,31 @@ public class TraitTestData {
         TraitResponseDTO trait2 = new TraitResponseDTO(
                 UUID.fromString("6a3e9932-4802-4815-8de8-2f1e99bdf842"),
                 TRAIT_TYPE,
-                TRAIT_NAME
+                "Trait 2"
         );
         TraitResponseDTO trait3 = new TraitResponseDTO(
                 UUID.fromString("6a3e9932-4802-4815-8de8-2f1e99bdf843"),
                 TRAIT_TYPE,
-                TRAIT_NAME
+                "Trait 3"
         );
 
         List<TraitResponseDTO> traits = List.of(trait1, trait2, trait3);
 
         return new PageImpl<>(traits);
+    }
+
+    public static TraitInsertRequestDTO getTraitInsertRequestDTO() {
+        return new TraitInsertRequestDTO(
+                TRAIT_TYPE,
+                TRAIT_NAME
+        );
+    }
+
+    public static TraitUpdateRequestDTO getTraitUpdateRequestDTO() {
+        return new TraitUpdateRequestDTO(
+                TRAIT_ID,
+                TRAIT_TYPE,
+                TRAIT_NAME
+        );
     }
 }
