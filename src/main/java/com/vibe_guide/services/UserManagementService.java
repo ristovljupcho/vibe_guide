787250439d8e114fc44a69b01e526c294e6c18f0
Dzoki12 <petrovski.gjorgji21@gmail.com>
2025-04-12 03:30:10 +0200

    Corrected Implementation of the user methods

--- src/main/java/com/vibe_guide/services/UserManagementService.java
+++ src/main/java/com/vibe_guide/services/UserManagementService.java
@@ -3,6 +3,8 @@ package com.vibe_guide.services;
 import com.vibe_guide.dtos.UserChangePasswordRequestDTO;
 import com.vibe_guide.dtos.UserLoginRequestDTO;
 import com.vibe_guide.dtos.UserRegisterRequestDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.UUID;
 
@@ -10,7 +12,7 @@ public interface UserManagementService {
 
     String userRegister(UserRegisterRequestDTO registerRequestDTO);
 
-    String userLogin(UserLoginRequestDTO userLoginRequestDTO);
+    String userLogin(UserLoginRequestDTO userLoginRequestDTO, HttpServletRequest request, HttpServletResponse response);
 
     String userLogout(UUID userId);
 
--- src/main/java/com/vibe_guide/services/impl/UserManagementServiceImpl.java
+++ src/main/java/com/vibe_guide/services/impl/UserManagementServiceImpl.java
@@ -1,25 +1,27 @@
 package com.vibe_guide.services.impl;
 
-//import com.vibe_guide.config.jwt.JwtBlacklistService;
+import com.vibe_guide.config.jwt.JWTService;
 import com.vibe_guide.dtos.UserChangePasswordRequestDTO;
 import com.vibe_guide.dtos.UserLoginRequestDTO;
 import com.vibe_guide.dtos.UserRegisterRequestDTO;
 import com.vibe_guide.entities.User;
 import com.vibe_guide.enums.Role;
 import com.vibe_guide.exceptions.UserInvalidCredentialsException;
-import com.vibe_guide.exceptions.UserEmailAlreadyExistsException;
 import com.vibe_guide.exceptions.UserNotFoundException;
-import com.vibe_guide.exceptions.UsernameAlreadyTakenException;
 import com.vibe_guide.repositories.UserRepository;
 import com.vibe_guide.services.UserManagementService;
 import com.vibe_guide.utils.UserResponseMessages;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -28,89 +30,77 @@ public class UserManagementServiceImpl implements UserManagementService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-//    private final JwtBlacklistService jwtBlacklistService;
+    private final JWTService jwtService;
 
     @Transactional
     @Override
     public String userRegister(UserRegisterRequestDTO registerRequestDTO) {
-        checkIfUserExistsByUsernameAndEmail(registerRequestDTO.username(), registerRequestDTO.email());
 
         User user = new User();
         user.setUsername(registerRequestDTO.username());
         user.setEmail(registerRequestDTO.email());
         user.setName(registerRequestDTO.name());
         user.setRole(Role.USER);
-        String password = passwordEncoder.encode(registerRequestDTO.password());
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(registerRequestDTO.password()));
         user.setCreatedAt(LocalDateTime.now());
         userRepository.save(user);
         return UserResponseMessages.USER_REGISTER_MESSAGE;
     }
 
+    @Transactional
     @Override
-    public String userLogin(UserLoginRequestDTO userLoginRequestDTO) {
-        Optional<User> userOptional = userRepository.findByUsername(userLoginRequestDTO.username());
-        if(userOptional.isEmpty()) {
-            throw new UserNotFoundException(userLoginRequestDTO.username());
-        }
+    public String userLogin(UserLoginRequestDTO userLoginRequestDTO, HttpServletRequest request, HttpServletResponse response) {
+        User user = userRepository.findByUsername(userLoginRequestDTO.username())
+                .orElseThrow(() -> new UserNotFoundException(userLoginRequestDTO.username()));
 
-        User user = userOptional.get();
-
-        if (!passwordEncoder.matches(userLoginRequestDTO.password(), user.getPassword())) {//private method
+        if (!passwordEncoder.matches(userLoginRequestDTO.password(), user.getPassword())) {
             throw new UserInvalidCredentialsException(userLoginRequestDTO.username());
         }
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtService.generateToken(user.getUsername());
+        response.setHeader("Authorization", "Bearer " + token);
+        request.getSession().setAttribute("user", user);
         return UserResponseMessages.USER_LOGIN_MESSAGE;
     }
 
+    @Transactional
     @Override
     public String userLogout(UUID userId) {
+        SecurityContextHolder.clearContext();
         return UserResponseMessages.USER_LOGOUT_MESSAGE;
     }
 
+    @Transactional
     @Override
     public String changePassword(UserChangePasswordRequestDTO changePasswordRequestDTO) {
-        return userRepository.findById(changePasswordRequestDTO.userId())
-                .map(user -> {
-                    String username = user.getUsername();
-
-                    if (!passwordEncoder.matches(changePasswordRequestDTO.currentPassword(), user.getPassword())) {
-                        throw new UserInvalidCredentialsException(username);
-                    }
-
-                    if (!changePasswordRequestDTO.newPassword().equals(changePasswordRequestDTO.confirmNewPassword())) {
-                        throw new IllegalArgumentException("Passwords do not match");
-                    }
-
-                    if (changePasswordRequestDTO.newPassword().length() < 8) {
-                        throw new IllegalArgumentException("New password must be at least 8 characters long for user:" +
-                                " " + username);//remove
-                    }
-
-                    user.setPassword(passwordEncoder.encode(changePasswordRequestDTO.newPassword()));
-                    userRepository.save(user);
-
-                    return UserResponseMessages.USER_CHANGE_PASSWORD_MESSAGE;
-                })
+        User user = userRepository.findById(changePasswordRequestDTO.userId())
                 .orElseThrow(() -> new UserNotFoundException(changePasswordRequestDTO.userId()));
+
+        validatePasswordChange(user, changePasswordRequestDTO);
+        user.setPassword(passwordEncoder.encode(changePasswordRequestDTO.newPassword()));
+        userRepository.save(user);
+        return UserResponseMessages.USER_CHANGE_PASSWORD_MESSAGE;
     }
 
-
+    @Transactional
     @Override
     public String deleteUser(UUID userId, String password) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
         userRepository.delete(user);
         return UserResponseMessages.USER_DELETE_MESSAGE;
     }
 
-
-    private void checkIfUserExistsByUsernameAndEmail(String username , String email) {
-        Optional<User> userOptionalUsername = userRepository.findByUsername(username);
-        Optional<User> userOptionalEmail = userRepository.findByEmail(email);
-        if(userOptionalUsername.isPresent()){
-            throw new UsernameAlreadyTakenException(username);
+    private void validatePasswordChange(User user, UserChangePasswordRequestDTO changePasswordRequestDTO) {
+        if (!passwordEncoder.matches(changePasswordRequestDTO.currentPassword(), user.getPassword())) {
+            throw new UserInvalidCredentialsException(user.getUsername());
         }
-        if(userOptionalEmail.isPresent()){
-            throw new UserEmailAlreadyExistsException(email);
+        if (!changePasswordRequestDTO.newPassword().equals(changePasswordRequestDTO.confirmNewPassword())) {
+            throw new IllegalArgumentException("Passwords do not match");
         }
     }
 }
+
