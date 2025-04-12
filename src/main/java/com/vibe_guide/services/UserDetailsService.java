305c0badf288f706e95edafcbd12fce1bc6aa8ea
Dzoki12 <petrovski.gjorgji21@gmail.com>
2025-04-12 03:25:32 +0200

    Reconstructed jwt token authentication logic

---
+++ src/main/java/com/vibe_guide/services/UserDetailsService.java
@@ -0,0 +1,8 @@
+package com.vibe_guide.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
