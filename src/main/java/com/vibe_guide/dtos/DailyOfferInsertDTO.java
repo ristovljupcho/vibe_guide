b0922240657b584b6a470823985754137902a417
Negrioska <leona.negrioska@gmail.com>
2025-04-13 16:58:25 +0200

    Changed annotation @NotNull instead of @NotEmpty

--- src/main/java/com/vibe_guide/dtos/DailyOfferInsertDTO.java
+++ src/main/java/com/vibe_guide/dtos/DailyOfferInsertDTO.java
@@ -1,15 +1,16 @@
 package com.vibe_guide.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record DailyOfferInsertDTO(
         @NotEmpty
         String name,
-        @NotEmpty
+        @NotNull
         LocalDateTime startDate,
-        @NotEmpty
+        @NotNull
         LocalDateTime endDate,
         @NotEmpty
         String description,
