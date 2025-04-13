8ac0625a5b04eb0b3dd02fa4023f35a2adaa482f
Negrioska <leona.negrioska@gmail.com>
2025-04-13 16:43:03 +0200

    Formated file.

--- src/main/java/com/vibe_guide/utils/EventGalleryResponseMessages.java
+++ src/main/java/com/vibe_guide/utils/EventGalleryResponseMessages.java
@@ -4,7 +4,7 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class EventGalleryResponseMessages {
-    public static final String EVENT_GALLERY_INSERT_MESSAGE = "Gallery for event successfully inserted.";
     public static final String EVENT_GALLERY_DELETE_MESSAGE = "Gallery for event successfully deleted.";
-    public static final String EVENT_GALLERY_DELETE_PHOTO_MESSAGE = "Photo from gallery for event successfully deleted.";
+    public static final String EVENT_GALLERY_DELETE_PHOTO_MESSAGE =
+            "Photo from gallery for event successfully deleted.";
 }
