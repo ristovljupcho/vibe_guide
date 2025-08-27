08616c156867fad23ad1b15a3b39532fddc3860d
ristovljupcho <ristov.ljupcho@gmail.com>
2025-08-27 12:37:06 +0200

    Added new methods to EventController.

--- src/main/java/com/vibe_guide/controllers/EventController.java
+++ src/main/java/com/vibe_guide/controllers/EventController.java
@@ -1,14 +1,21 @@
 package com.vibe_guide.controllers;
 
 
+import com.vibe_guide.dtos.EventInsertRequestDTO;
 import com.vibe_guide.dtos.EventResponseDTO;
+import com.vibe_guide.dtos.EventUpdateRequestDTO;
 import com.vibe_guide.services.EventManagementService;
 import com.vibe_guide.services.EventQueryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +55,27 @@ public class EventController {
     @GetMapping("/{placeId}/todays")
     ResponseEntity<List<EventResponseDTO>> getTodaysEventsByPlaceId(@PathVariable UUID placeId) {
         List<EventResponseDTO> response = eventQueryService.findTodaysEventsByPlaceId(placeId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/insert")
+    ResponseEntity<String>  insertEvent(@RequestBody @Valid EventInsertRequestDTO dto) {
+        String response = eventManagementService.insertEvent(dto);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/update")
+    ResponseEntity<String> updateEvent(@RequestBody @Valid EventUpdateRequestDTO dto) {
+        String response = eventManagementService.updateEvent(dto);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{eventId}/delete")
+    ResponseEntity<String> deleteEvent(@PathVariable UUID eventId) {
+        String response = eventManagementService.deleteEvent(eventId);
 
         return ResponseEntity.ok(response);
     }
