f87778151dec872203387da2a67ed917e8ecab05
ristov.ljupcho <ristov.ljupcho@gmail.com>
2025-12-03 14:05:09 +0100

    Changed trait priority from boolean to enum TraitPriority.

--- src/main/java/com/vibe_guide/dtos/PlaceTraitRequestDTO.java
+++ src/main/java/com/vibe_guide/dtos/PlaceTraitRequestDTO.java
@@ -1,5 +1,6 @@
 package com.vibe_guide.dtos;
 
+import com.vibe_guide.enums.TraitPriority;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -13,6 +14,6 @@ public record PlaceTraitRequestDTO(
         @NotBlank
         String additionalInformation,
         @NotNull
-        boolean priority
+        TraitPriority priority
 ) {
 }
